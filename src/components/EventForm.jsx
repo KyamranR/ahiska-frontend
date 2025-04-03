7e1deabdb150de948565f5af76b32ce2026302cf
Kyamran <kyamranr@gmail.com>
2025-04-03 19:21:11 -0400

    renamed date and time

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AhiskaApi from "../api/AhiskaApi";
 import { useAuth } from "../context/AuthContext";
 
@@ -7,11 +7,26 @@ const EventForm = ({ onEventCreated, eventToEdit = null }) => {
   const [eventData, setEventData] = useState({
     title: "",
     description: "",
-    date: "",
-    time: "",
+    event_date: "",
+    event_time: "",
     location: "",
     created_by: currentUser.id,
   });
+
+  useEffect(() => {
+    if (eventToEdit) {
+      setEventData({
+        title: eventToEdit.title,
+        description: eventToEdit.description,
+        event_date: eventToEdit.event_date
+          ? eventToEdit.event_date.slice(0, 10)
+          : "",
+        event_time: eventToEdit.time,
+        location: eventToEdit.location,
+        created_by: currentUser.id,
+      });
+    }
+  }, [eventToEdit, currentUser.id]);
 
   const handleChange = (e) => {
     setEventData({ ...eventData, [e.target.name]: e.target.value });
@@ -20,10 +35,15 @@ const EventForm = ({ onEventCreated, eventToEdit = null }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await AhiskaApi.createEvent(eventData);
+      if (eventToEdit) {
+        await AhiskaApi.updateEvent(eventToEdit.id, eventData);
+      } else {
+        await AhiskaApi.createEvent(eventData);
+      }
+
       onEventCreated();
     } catch (error) {
-      console.error("Error creating event:", error);
+      console.error("Error creating/updating event:", error);
     }
   };
 
@@ -58,8 +78,8 @@ const EventForm = ({ onEventCreated, eventToEdit = null }) => {
         </label>
         <input
           type="date"
-          name="date"
-          value={eventData.date}
+          name="event_date"
+          value={eventData.event_date}
           onChange={handleChange}
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
@@ -69,9 +89,9 @@ const EventForm = ({ onEventCreated, eventToEdit = null }) => {
           Time:
         </label>
         <input
-          type="time"
-          name="time"
-          value={eventData.time}
+          type="text"
+          name="event_time"
+          value={eventData.event_time}
           onChange={handleChange}
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
